6c8800c61513b6b6f1855c9c31c8feb2f78237ee
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2024-12-05 22:06:11 +0700

    complete lab

--- week5/question10.cpp
+++ week5/question10.cpp
@@ -8,8 +8,35 @@ using namespace std;
 
 int Dijkstra(int** graph, int src, int dst) {
 	// TODO: return the length of shortest path from src to dst.
+    const int n = 6; // since n not define, we set it to const 6
 
+    vector<int> dist(n, INT_MAX);
+    vector<bool> visited(n, false);
 
+    dist[src] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    pq.push(make_pair(dist[src], src));
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (visited[u]) continue;
+
+        visited[u] = true;
+        for (int v = 0; v < n; ++v) {
+            if (graph[u][v] > 0) {
+                int weight = graph[u][v];
+                if (!visited[v] && dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                    pq.push(make_pair(dist[v], v));
+                }
+            }
+        }
+    }
+
+    return dist[dst];
 }
 
 int main() {
